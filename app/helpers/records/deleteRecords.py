6fa228fc8242878700f93ec151c6376069aaffbc
nidhogg <j.carlosglxg@gmail.com>
2021-08-20 15:14:52 -0500

    feature(DataBase): add a database for future analysis

--- app/helpers/records/deleteRecords.py
+++ app/helpers/records/deleteRecords.py
@@ -6,7 +6,7 @@ from app.models.entryORM import db
 def delete_records() -> NoReturn:
     db.drop_all()
     # Code for postgresql
-    # for table in db.metadata.tables.keys():
-    #     db.session.execute(f"TRUNCATE TABLE {table} CASCADE")
-    #     db.session.execute(f"ALTER SEQUENCE {table}_id_seq RESTART WITH 1")
-    # db.session.commit()
+    # for table in database.metadata.tables.keys():
+    #     database.session.execute(f"TRUNCATE TABLE {table} CASCADE")
+    #     database.session.execute(f"ALTER SEQUENCE {table}_id_seq RESTART WITH 1")
+    # database.session.commit()

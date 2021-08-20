0cb92850de8cfada8f4353d14fc61ad5c06c7c02
nidhogg <j.carlosglxg@gmail.com>
2021-08-20 14:08:01 -0500

    start the initial project

--- app/config/configApp.py
+++ app/config/configApp.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from app.config.configDB import DBConfig
 
 
 @dataclass
 class AppConfig(object):
     DEBUG: bool = True
     TESTING: bool = True
+    SQLALCHEMY_DATABASE_URI: str = DBConfig.PATH
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     APPLICATION_ROOT: str = "/api"
 

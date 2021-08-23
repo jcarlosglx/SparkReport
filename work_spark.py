7ab28e8382b5b0be9bfd55ac977b3b54b9564cb5
nidhogg <j.carlosglxg@gmail.com>
2021-08-23 17:55:55 -0500

    clean the code and union

--- work_spark.py
+++ work_spark.py
@@ -14,11 +14,11 @@ with SparkDataFrame("FinancialApp") as spark_df:
         .option("header", True).option("delimiter", ",")\
         .schema(stock_schema)\
         .load(path_db)
-    file_df.printSchema()
-    file_df.show(truncate=True)
+    # file_df.printSchema()
+    # file_df.show(truncate=True)
 
     stock_df = file_df.select("Date", "AMZN")
     pd_df = stock_df.toPandas()
     with IMGFile() as path_plot:
         create_plot(pd_df, path_plot)
-        single_report_pdf("font-colors.pdf", path_plot)
+        single_report_pdf(pd_df, "font-colors.pdf", path_plot)

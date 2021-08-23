5caeff853fda99f1e1d48ff194a42ee0e53ba179
nidhogg <j.carlosglxg@gmail.com>
2021-08-23 18:03:32 -0500

    feature(Schema): add a schema for read the csv file

---
+++ app/schemas/stockSchema.py
@@ -0,0 +1,17 @@
+from pyspark.sql.types import StructType, StructField, DateType, FloatType
+
+
+stock_schema = StructType(
+        [
+            StructField("Date", DateType(), True),
+            StructField("AAPL", FloatType(), True),
+            StructField("BA", FloatType(), True),
+            StructField("T", FloatType(), True),
+            StructField("MGM", FloatType(), True),
+            StructField("AMZN", FloatType(), True),
+            StructField("IBM", FloatType(), True),
+            StructField("TSLA", FloatType(), True),
+            StructField("GOOG", FloatType(), True),
+            StructField("sp500", FloatType(), True),
+        ]
+    )

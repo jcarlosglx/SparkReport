f51d65509a068123b0a57b3ab31d60d81a4ba207
nidhogg <j.carlosglxg@gmail.com>
2021-08-23 15:35:13 -0500

    Working with the visualitation

--- work_spark.py
+++ work_spark.py
@@ -1,10 +1,11 @@
 from app.ia.spark.sparkDF import SparkDataFrame
-from pyspark.sql.types import StringType, StructType, StructField, DateType, FloatType
-
+from pyspark.sql.types import StructType, StructField, DateType, FloatType
+import matplotlib.pyplot as plt
 from os.path import abspath, dirname, join
 
 path_base = abspath(dirname(__file__))
 path_db = join(path_base, "app/database/stock.csv")
+
 with SparkDataFrame("FinancialApp") as spark_df:
     #2012 - 01 - 12,
     schema = StructType(
@@ -22,11 +23,19 @@ with SparkDataFrame("FinancialApp") as spark_df:
         ]
     )
 
-    data_file_df = spark_df.spark_session.read\
+    file_df = spark_df.spark_session.read\
         .format("csv")\
         .option("header", True).option("delimiter", ",")\
         .schema(schema)\
         .load(path_db)
-    data_file_df.printSchema()
-    data_file_df.show(truncate=True)
+    file_df.printSchema()
+    file_df.show(truncate=True)
 
+    stock_df = file_df.select("Date", "AMZN")
+    pandas_df = file_df.toPandas()
+    pandas_df.plot(x="Date", y="AMZN", label="AMAZN stock Price", linewidth=3)
+    plt.ylabel("Price")
+    plt.title("Plotting Stocks")
+    plt.legend(loc="upper left")
+    plt.grid()
+    plt.show()

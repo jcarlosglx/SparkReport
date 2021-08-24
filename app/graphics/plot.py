a9f9c19abb30f9eddee3d4e313d0d60defc8cb91
nidhogg <j.carlosglxg@gmail.com>
2021-08-24 15:00:37 -0500

    refact(Graphics): use matplotlib in replace of pandas

--- app/graphics/plot.py
+++ app/graphics/plot.py
@@ -3,10 +3,10 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 
 
-def create_plot(pandas_df: DataFrame, path_img: str) -> NoReturn:
-    x_name = pandas_df.columns[0]
-    y_name = pandas_df.columns[1]
-    pandas_df.plot(x=x_name, y=y_name, label=f"{y_name} stock price", linewidth=3)
+def create_plot(pandas_x_df: DataFrame, pandas_y_df: DataFrame, path_img: str) -> NoReturn:
+    x_name = pandas_x_df.columns[0]
+    y_name = pandas_y_df.columns[0]
+    plt.plot(pandas_x_df, pandas_y_df, label=f"{y_name} stock price in {x_name}", linewidth=3)
     plt.ylabel("Price")
     plt.title("Plotting Stocks")
     plt.legend(loc="upper left")

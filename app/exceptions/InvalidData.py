af5ad44bd3d73742b0be8ba27267be03a3425662
nidhogg <j.carlosglxg@gmail.com>
2021-08-31 09:45:52 -0500

    feature(Exception): add two exceptions

---
+++ app/exceptions/InvalidData.py
@@ -0,0 +1,10 @@
+from werkzeug.exceptions import HTTPException
+from app.messages.errorMessages import ERROR_MSG_INVALID_DATA
+from app.messages.statusMessages import STATUS_424
+
+
+class InvalidData(HTTPException):
+    code = int(STATUS_424)
+    description = (
+        ERROR_MSG_INVALID_DATA
+    )
--- app/messages/errorMessages.py
+++ app/messages/errorMessages.py
@@ -1,4 +1,6 @@
 ERROR_MSG_500 = "Error: Server"
+ERROR_MSG_INVALID_GRAPHIC = "Error: Not a valid graphic to show"
+ERROR_MSG_INVALID_DATA = "Error: Not a valid combination of data for show"
 ERROR_MSG_ID_NOT_FOUND = "Error: Id not found"
 ERROR_MSG_METHOD_NOT_ALLOW = "Error: Method not allow"
 ERROR_MSG_DB_INTEGRITY = "Error: Data unable to save"

fb63feafbd76b7cff6007dc1f1ae0f6d38cbd284
nidhogg <j.carlosglxg@gmail.com>
2021-08-31 09:49:01 -0500

    refact(Graphics): add a try-except and chenge the return value to bool

--- app/graphics/stockGraphics.py
+++ app/graphics/stockGraphics.py
@@ -28,49 +28,75 @@ class StockGraphics:
         plt.xlabel(x_name)
 
     def get_statistics(self, pandas_df: DataFrame) -> dict:
-        name = pandas_df.columns[0]
-        return {
-            "mean": pandas_df[name].mean(),
-            "median": pandas_df[name].median(),
-            "mode": round(pandas_df[name].mode()[0], 5),
-            "std": pandas_df[name].std(),
-            "max": pandas_df[name].max(),
-            "min": pandas_df[name].min()
-        }
+        try:
+            name = pandas_df.columns[0]
+            return {
+                "mean": pandas_df[name].mean(),
+                "median": pandas_df[name].median(),
+                "mode": round(pandas_df[name].mode()[0], 5),
+                "std": pandas_df[name].std(),
+                "max": pandas_df[name].max(),
+                "min": pandas_df[name].min()
+            }
+        except:
+            return {}
 
-    def create_histogram(self, pandas_df: DataFrame, path: str) -> NoReturn:
-        x_name = pandas_df.columns[0]
-        self.__single_template("Histogram Stock", pandas_df, pandas_df)
-        num_bins = 40
-        plt.hist(pandas_df[x_name], num_bins, facecolor="blue")
-        plt.savefig(path)
+    def create_histogram(self, pandas_df: DataFrame, path: str) -> bool:
+        try:
+            x_name = pandas_df.columns[0]
+            self.__single_template("Histogram Stock", pandas_df, pandas_df)
+            num_bins = 40
+            plt.hist(pandas_df[x_name], num_bins, facecolor="blue")
+            plt.savefig(path)
+            return True
+        except:
+            return False
 
-    def create_boxplot(self, pandas_df: DataFrame, path: str) -> NoReturn:
-        self.__single_template("Plotting Stocks", pandas_df, pandas_df)
-        plt.boxplot(pandas_df)
-        plt.savefig(path)
+    def create_boxplot(self, pandas_df: DataFrame, path: str) -> bool:
+        try:
+            self.__single_template("Plotting Stocks", pandas_df, pandas_df)
+            plt.boxplot(pandas_df)
+            plt.savefig(path)
+            return True
+        except:
+            return False
 
-    def create_multi_boxplot(self, pandas_df: DataFrame, path: str) -> NoReturn:
-        self.__multi_template("Plotting Stocks", pandas_df, pandas_df)
-        labels = [f"{name}" for name in pandas_df.columns]
-        plt.boxplot(pandas_df, labels=labels)
-        plt.savefig(path)
+    def create_multi_boxplot(self, pandas_df: DataFrame, path: str) -> bool:
+        try:
+            self.__multi_template("Plotting Stocks", pandas_df, pandas_df)
+            labels = [f"{name}" for name in pandas_df.columns]
+            plt.boxplot(pandas_df, labels=labels)
+            plt.savefig(path)
+            return True
+        except:
+            return False
 
-    def create_scatter(self, pandas_x_df: DataFrame, pandas_y_df: DataFrame, path: str) -> NoReturn:
-        self.__single_template("Stock compare", pandas_x_df, pandas_y_df)
-        plt.scatter(pandas_x_df, pandas_y_df)
-        plt.savefig(path)
+    def create_scatter(self, pandas_x_df: DataFrame, pandas_y_df: DataFrame, path: str) -> bool:
+        try:
+            self.__single_template("Stock compare", pandas_x_df, pandas_y_df)
+            plt.scatter(pandas_x_df, pandas_y_df)
+            plt.savefig(path)
+        except:
+            return False
 
-    def create_plot(self, pandas_x_df: DataFrame, pandas_y_df: DataFrame, path: str) -> NoReturn:
-        self.__single_template("Plotting Stocks", pandas_x_df, pandas_y_df)
-        name = pandas_y_df.columns[0]
-        plt.plot(pandas_x_df, pandas_y_df, label=name, linewidth=3)
-        plt.legend(loc="upper left")
-        plt.savefig(path)
+    def create_plot(self, pandas_x_df: DataFrame, pandas_y_df: DataFrame, path: str) -> bool:
+        try:
+            self.__single_template("Plotting Stocks", pandas_x_df, pandas_y_df)
+            name = pandas_y_df.columns[0]
+            plt.plot(pandas_x_df, pandas_y_df, label=name, linewidth=3)
+            plt.legend(loc="upper left")
+            plt.savefig(path)
+            return True
+        except:
+            return False
 
-    def create_multi_plot(self, pandas_x_df: DataFrame, pandas_y_df: DataFrame, path: str) -> NoReturn:
-        self.__multi_template("Plotting Stocks", pandas_x_df, pandas_y_df)
-        name = pandas_y_df.columns
-        plt.plot(pandas_x_df, pandas_y_df, label=name, linewidth=3)
-        plt.legend(loc="upper left")
-        plt.savefig(path)
+    def create_multi_plot(self, pandas_x_df: DataFrame, pandas_y_df: DataFrame, path: str) -> bool:
+        try:
+            self.__multi_template("Plotting Stocks", pandas_x_df, pandas_y_df)
+            name = pandas_y_df.columns
+            plt.plot(pandas_x_df, pandas_y_df, label=name, linewidth=3)
+            plt.legend(loc="upper left")
+            plt.savefig(path)
+            return True
+        except:
+            return False

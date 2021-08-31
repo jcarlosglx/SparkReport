91b778ac7ffb9b4032e159d6df5a420426f523d9
nidhogg <j.carlosglxg@gmail.com>
2021-08-31 10:53:27 -0500

    refact(Controller): refact of base controller

--- app/controllers/baseController.py
+++ app/controllers/baseController.py
@@ -13,6 +13,7 @@ from app.config.configGraphics import TypesGraphics
 from app.schemas.stockSchema import stock_schema
 from app.config.configDB import DBConfig
 from app.reports.baseReport import BaseReport
+from pandas import DataFrame
 
 
 class BaseController:
@@ -20,39 +21,57 @@ class BaseController:
     schema: Type[Schema]
     report: BaseReport
 
+    def are_create_graphics(self, result_graph: List[bool]) -> bool:
+        for result in result_graph:
+            if not result:
+                return False
+        return True
+
+    def get_spark_record(self, types_graphics: List[str]) -> Dict[str]:
+        with SparkDataFrame() as spark_df:
+            dict_data = {}
+            file_df = spark_df.spark_session.read \
+                .format("csv") \
+                .option("header", True).option("delimiter", ",") \
+                .schema(stock_schema) \
+                .load(DBConfig.STOCK_DB)
+            has_x = "x" in types_graphics
+            has_y = "y" in types_graphics
+            has_statistics = "statistics" in types_graphics
+            if has_x:
+                dict_data["x"] = file_df.select(self.data_json["x"]).toPandas()
+            if has_y:
+                dict_data["y"] = file_df.select(self.data_json["y"]).toPandas()
+            if has_statistics:
+                dict_data["statistics"] = True
+        return dict_data
+
+    def create_graphics(self, paths: List[str], data: Dict[str]) -> List[bool]:
+        results = []
+        stock_graphic = StockGraphics()
+        keys = data.keys()
+        for i, graphic in enumerate(paths):
+            if "x" in keys:
+                method = getattr(stock_graphic, graphic)
+                results.append(method(data["y"], paths[i]))
+            elif ("x" in keys) and ("y" in keys):
+                method = getattr(stock_graphic, graphic)
+                results.append(method(data["x"], data["y"], paths[i]))
+        return results
+
     def get_report(self) -> Response:
         self.data_json = request.get_json()
         schema = self.schema()
         schema.load(self.data_json, partial=True)
         types_graphics = self.data_json.keys()
         with PathFiles(len(types_graphics)) as paths:
-            with SparkDataFrame() as spark_df:
-                stock_graphic = StockGraphics()
-                file_df = spark_df.spark_session.read \
-                    .format("csv") \
-                    .option("header", True).option("delimiter", ",") \
-                    .schema(stock_schema) \
-                    .load(DBConfig.STOCK_DB)
-                has_x = "x" in types_graphics
-                has_y = "y" in types_graphics
-                has_statistics = "statistics" in types_graphics
-                other_data = None
-                if has_x:
-                    stock_x_df = file_df.select(self.data_json["x"]).toPandas()
-                if has_y:
-                    stock_y_df = file_df.select(self.data_json["y"]).toPandas()
-
-                for i, graphic in enumerate(paths):
-                    if not has_x:
-                        method = getattr(stock_graphic, graphic)
-                        method(stock_y_df, paths[i])
-                    elif has_x and has_y:
-                        method = getattr(stock_graphic, graphic)
-                        method(stock_x_df, stock_y_df, paths[i])
-                    if has_statistics:
-                        other_data = stock_graphic.statistics(stock_y_df)
-
-                self.report.create_report(stock_y_df, "single.pdf", paths, other_data)
-
+            data = self.get_spark_record(types_graphics)
+            keys = data.keys()
+            results = self.create_graphics(paths, data)
+            if "statistics" in keys:
+                other_data = StockGraphics().statistics(data["y"])
+            if self.are_create_graphics(results):
+                self.report.create_report(data["y"], "single.pdf", paths, other_data)
                 return MessageReturn().access_record_message("")
+        return HandlerError.handler_middleware_error(InvalidGraphic())
 

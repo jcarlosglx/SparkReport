51e9d0e321c00d05dc6ce649c7bc81b81fea348e
nidhogg <j.carlosglxg@gmail.com>
2021-08-31 09:47:04 -0500

    feature(Schemas): add two schemas

---
+++ app/schemas/singleReporteSchema.py
@@ -0,0 +1,15 @@
+from marshmallow import Schema, fields
+from typing import Dict, List
+
+
+class SingleReportSchema(Schema):
+
+    histogram: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    plot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    boxplot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    scatter: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    multi_boxplot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.List(fields.String()))
+    multi_plot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.List(fields.String()))
+    statistics: bool = fields.Boolean()
+    x: str = fields.String(required=True)
+    y: List[str] = fields.List(fields.String(), required=True)
---
+++ app/schemas/squareReporteSchema.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+from typing import Dict, List
+
+
+class SquareReportSchema(Schema):
+    histogram: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    plot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    boxplot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    scatter: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.String())
+    multi_boxplot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.List(fields.String()))
+    multi_plot: Dict[str, str] = fields.Dict(keys=fields.String(), values=fields.List(fields.String()))
+    statistics: bool = fields.Boolean()
+    x: str = fields.String(required=True)
+    y: List[str] = fields.List(fields.String(), required=True)

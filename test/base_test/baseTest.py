910029ccc35f0935e2660e2271d101a8e4a37360
nidhogg <j.carlosglxg@gmail.com>
2021-09-02 21:07:09 -0500

    refact(Test): change the code to be adapted to the project

--- test/base_test/baseTest.py
+++ test/base_test/baseTest.py
@@ -1,10 +1,10 @@
 from test.config.configTest import ConfigTest
-from test.helpers.dummy_json_test.dummy_json_test import get_dummy_json_test
 from typing import Type
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema
+from app.schemas.stockSchema import stock_schema
 
 from app.messages.statusMessages import STATUS_200
 
@@ -15,13 +15,18 @@ class BaseGetGeneralTest:
     endpoint_get: str
     response_key: str = ConfigTest.response_key
     schema_general_get: Type[Schema]
+    header_cvs = list(stock_schema.fieldNames())
 
-    def test_get_general(self, get_app: Flask, get_db: Type[SQLAlchemy]):
-        json_data = get_dummy_json_test(self.schema_general_get)
+    def test_histogram(self, get_app: Flask, get_db: Type[SQLAlchemy]):
+        json_data = {
+            "x": self.header_cvs[1],
+            "statistics": True
+        }
         response = get_app.test_client().get(
             f"{self.url_get}{self.endpoint_get}", json=json_data
         )
         code_response = response.get_json()[self.response_key]
         assert (
-            code_response == self.expect_status_get
+                code_response == self.expect_status_get
         ), f"Expected {self.expect_status_get} got {code_response}"
+
--- test/test_single_report/test_single_report.py
+++ test/test_single_report/test_single_report.py
@@ -1,10 +1,10 @@
 from test.base_test.baseTest import BaseGetGeneralTest
-
+from app.config.configEndpoint import EndpointConfig
 from app.messages.statusMessages import STATUS_200
 from app.schemas.singleReporteSchema import SingleReportSchema
 
 
-class TestMiddleware(BaseGetGeneralTest):
-    endpoint_get = "ThisEndpointShouldNotExist"
+class TestSingleReport(BaseGetGeneralTest):
+    endpoint_get = EndpointConfig.endpoint_single
     expect_status_get = STATUS_200
     schema_general_get = SingleReportSchema

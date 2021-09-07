b206cc9c7c3645dae316c8271d490a73cde11525
nidhogg <j.carlosglxg@gmail.com>
2021-09-07 14:55:11 -0500

    fix(Test): Fix the test of get the file with Histogram

--- test/base_test/baseTest.py
+++ test/base_test/baseTest.py
@@ -61,4 +61,32 @@ class BaseGetGeneralTest:
                 code_response == self.expect_status_get
         ), self.print_error(code_response)
 
+    def test_boxplot(self, get_app: Flask, get_db: Type[SQLAlchemy]):
+        json_data = {
+            "x": self.header_cvs[1],
+            "boxplot": [self.header_cvs[2]]
+        }
+        response = get_app.test_client().get(
+            f"{self.url_get}{self.endpoint_get}", json=json_data
+        )
+        if self.save_file:
+            self.save_response_file(response)
+        code_response = str(response.status_code)
+        assert (
+                code_response == self.expect_status_get
+        ), self.print_error(code_response)
 
+    def test_multi_boxplot(self, get_app: Flask, get_db: Type[SQLAlchemy]):
+        json_data = {
+            "x": self.header_cvs[1],
+            "multi_boxplot": [self.header_cvs[2]]
+        }
+        response = get_app.test_client().get(
+            f"{self.url_get}{self.endpoint_get}", json=json_data
+        )
+        if self.save_file:
+            self.save_response_file(response)
+        code_response = str(response.status_code)
+        assert (
+                code_response == self.expect_status_get
+        ), self.print_error(code_response)

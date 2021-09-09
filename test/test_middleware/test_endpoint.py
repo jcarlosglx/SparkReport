a7d38754ac47e932f33398b0e134cfc0eb779292
nidhogg <j.carlosglxg@gmail.com>
2021-09-08 23:21:17 -0500

    feature(Test): Finishi test middleware

--- test/test_middleware/test_endpoint.py
+++ test/test_middleware/test_endpoint.py
@@ -1,11 +1,8 @@
-# from test.base_test.baseTest import BaseGetGeneralTest, BasePutTest
-# from test.config.configTest import ConfigTest
-#
-# from app.messages.statusMessages import STATUS_405, STATUS_500
-#
-#
-# class TestMiddleware(BasePutTest, BaseGetGeneralTest):
-#     endpoint_put = ConfigTest.endpoint_logs
-#     endpoint_get = "ThisEndpointShouldNotExist"
-#     expect_status_get = STATUS_500
-#     expect_status_put = STATUS_405
+from test.base_test.baseHistogram import BaseGetHistogramTest
+
+from app.messages.statusMessages import STATUS_500
+
+
+class TestMiddleware(BaseGetHistogramTest):
+    endpoint_get = "ThisEndpointShouldNotExist"
+    expect_status_get = STATUS_500
